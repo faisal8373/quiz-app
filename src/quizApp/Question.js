77954a916707e758cc21ad9b1b9e49f590f2a625
faisal8373 <59798752+faisal8373@users.noreply.github.com>
2021-12-22 21:48:40 +0500

    finalized the functionality

--- src/quizApp/Question.js
+++ src/quizApp/Question.js
@@ -2,41 +2,13 @@ import React from 'react';
 import { useState } from 'react';
 import './Style.css'
 
-function Question(props) {
-    const [cor, setCor] = useState(0);
-    const [incor, setincor] = useState(0);
-
-    function onChangeHandle(event){
-        if(props.finished){
-            props.result.setCorrect(cor);
-            props.result.setIncorrect(incor)
-        }
-    if(event.target.value === props.question.options[props.question.correct]){
-        setCor(cor+1);
-        // console.log('correct', props.question.options[props.question.correct])
-    }
-    else{
-        setincor(incor+1);
-    }
-
-    // console.log(props.question.options[0])
-    }
+// function Question(props) {
+  
    
 
     
-    return (
-        <div>
-           <h1>{props.question.question}</h1>
+//     return ()
+        
+// }
 
-           {props.question.options.map((item, index)=>{
-  
-  return <div className='input' onChange={onChangeHandle}><input type="radio" id={item} name={props.question.question} value={item} />
-  <label for={item}>{item}</label> <br />
-  </div>
-})}
-          
-        </div>
-    )
-}
-
-export default Question
+// export default Question
--- src/quizApp/QuizApp.js
+++ src/quizApp/QuizApp.js
@@ -6,31 +6,61 @@ import { useState } from 'react';
 function QuizApp() {
 
 const [counter, setCounter] = useState(0);
-const [correct, setCorrect] = useState(0);
-const [inCorrect, setIncorrect] = useState(0);
+// const [correct, setCorrect] = useState(0);
+// const [inCorrect, setIncorrect] = useState(0);
 
 const [finished, setFinished] = useState(false)
+const [answer, setAnswer] = useState('');
+const [correct, setCorrect] = useState(0);
+const [incorrect, setIncorrect] = useState(0);
+
+
+
+    
+// console.log(props.question.options[0])
+
 
 function ctaHandler(){
-  
+  if(answer){
+  if(answer === dataSet[counter].options[dataSet[counter].correct]){
+    setCorrect(correct+1);
+    // console.log('correct', props.question.options[props.question.correct])
+    }
+    else{
+      setIncorrect(incorrect+1);
+    }
 if(counter+1 < dataSet.length){
 setCounter(counter+1);
 }
 else{
   setFinished(true);
+
 }
 
-console.log('counter', counter)
+  }
+
 }
     return (
         <div>
           <h1>JavaScript Quiz</h1>
-          <Question question={dataSet[counter]} result={[setCorrect, setIncorrect]} finish={finished}/>  
+          {/* <Question question={dataSet[counter]} result={[setCorrect, setIncorrect]} finish={finished}/>   */}
+          <div>
+           <h1>{dataSet[counter].question}</h1>
+
+           {dataSet[counter].options.map((item, index)=>{
+  
+  return <div className='input' onChange={(event) => {setAnswer(event.target.value)}}><input type="radio" id={item} name={dataSet[counter].question} value={item} />
+  <label for={item}>{item}</label> <br />
+  </div>
+})}
+          
+        </div>
+    
 
           {finished ? <p>Thank you for completing the test</p> : <></>}
-          <button onClick={ctaHandler}>Submit</button>
+          <button onClick={ctaHandler} disabled={finished}>Submit</button>
           <p>Correct: {correct}</p>
-          <p>Incorrect: {inCorrect}</p>
+          <p>Incorrect: {incorrect}</p>
 
         </div>
     )
